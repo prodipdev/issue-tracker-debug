b6aa3b6037f630dcde5ad91c7eac877248ca7d16
prodipdev <prodipink@gmail.com>
2023-03-12 20:56:06 +0600

    Debug Issue-Tracker and fixed errors.

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
+// Submit/Add button Listener
 document.getElementById('issueInputForm').addEventListener('submit', submitIssue);
 
+// Submit Methods
 function submitIssue(e) {
+  // Get Input Value
   const getInputValue = id => document.getElementById(id).value;
   const description = getInputValue('issueDescription');
   const severity = getInputValue('issueSeverity');
@@ -8,11 +11,20 @@ function submitIssue(e) {
   const id = Math.floor(Math.random()*100000000) + '';
   const status = 'Open';
 
-  const issue = { id, description, severity, assignedTo, status };
-  let issues = [];
-  if (localStorage.getItem('issues')){
-    issues = JSON.parse(localStorage.getItem('issues'));
+  // Simple input validation
+  if (!description || !assignedTo) {
+    if (!description && !assignedTo) {
+      return alert("Please type the 'Description' and 'Assigned To' fields!")
+    } else if (!description) {
+      return alert("Please type the 'Description' field!")
+    } else {
+      return alert("Please type the 'Assigned To' field!")
+    }
   }
+
+  const issue = { id, description, severity, assignedTo, status };
+
+  let issues = JSON.parse(localStorage.getItem('issues')) || [];
   issues.push(issue);
   localStorage.setItem('issues', JSON.stringify(issues));
 
@@ -21,36 +33,45 @@ function submitIssue(e) {
   e.preventDefault();
 }
 
+// Close Issue Methods
 const closeIssue = id => {
   const issues = JSON.parse(localStorage.getItem('issues'));
-  const currentIssue = issues.find(issue => issue.id === id);
-  currentIssue.status = 'Closed';
+  const currentIssue = issues.find(issue => issue.id == id);
+  // Check current status and replace
+  if (currentIssue.status === 'Open') {
+    currentIssue.status = 'Closed';
+  } else {
+    currentIssue.status = 'Open';
+  }
+  // event.target.remove()
   localStorage.setItem('issues', JSON.stringify(issues));
   fetchIssues();
 }
 
+// Delete Issue Methods
 const deleteIssue = id => {
   const issues = JSON.parse(localStorage.getItem('issues'));
-  const remainingIssues = issues.filter( issue.id !== id )
+  const remainingIssues = issues.filter( issue => issue.id != id )
   localStorage.setItem('issues', JSON.stringify(remainingIssues));
+  fetchIssues();
 }
 
+// Fetch Local Storage
 const fetchIssues = () => {
-  const issues = JSON.parse(localStorage.getItem('issues'));
+  const issues = JSON.parse(localStorage.getItem('issues')) || [];
   const issuesList = document.getElementById('issuesList');
   issuesList.innerHTML = '';
 
-  for (var i = 0; i < issues.length; i++) {
-    const {id, description, severity, assignedTo, status} = issues[i];
-
-    issuesList.innerHTML +=   `<div class="well">
-                              <h6>Issue ID: ${id} </h6>
-                              <p><span class="label label-info"> ${status} </span></p>
-                              <h3> ${description} </h3>
-                              <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
-                              <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
-                              <a href="#" onclick="setStatusClosed(${id})" class="btn btn-warning">Close</a>
-                              <a href="#" onclick="deleteIssue(${id})" class="btn btn-danger">Delete</a>
-                              </div>`;
-  }
+  issues?.forEach(issue => {
+      const {id, description, severity, assignedTo, status} = issue;
+      issuesList.innerHTML +=   `<div class="well">
+                                <h6>Issue ID: ${id} </h6>
+                                <p><span ${status === 'Closed' ? `class="label label-warning"` : ''} class="label label-success"> ${status} </span></p>
+                                <h3> ${description} </h3>
+                                <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
+                                <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
+                                ${status === 'Open' ? `<a href="#" onclick="closeIssue(${id})" class="btn btn-warning">Closed</a>` : `<a href="#" onclick="closeIssue(${id})" class="btn btn-success">Open</a>`}
+                                <a href="#" onclick="deleteIssue(${id})" class="btn btn-danger">Delete</a>
+                                </div>`;
+  })
 }
